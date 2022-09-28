99eee78aa2773e596b86d792359588ab5899f2d6
lqllqllql <103975573+lqllqllql@users.noreply.github.com>
2022-09-28 10:56:55 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -4,6 +4,11 @@ Model Module
 
 from . import steg_net
 
+# __xx：双下划线开头：私有变量
+# ----只有该类能调用该变量，其子类或其他类均不能调用
+
+# _xxx:单下划线开头：成员变量：
+# ----只有通过实例化后才能进行调用，不能通过from module import ***导入
 _models = [
     steg_net,
 ]
